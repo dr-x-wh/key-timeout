4e6b192205d5fe13c7f5cbe95a1632f267e21a01
dr-x <dr-x@mail.com>
2025-08-20 11:57:01 +0800

    fix: import

--- gunicorn.py
+++ gunicorn.py
@@ -1,16 +1,7 @@
 bind = "0.0.0.0:9009"
-workers = 4
-threads = 2
+workers = 1
+threads = 1
 accesslog = "logs/access.log"
 errorlog = "logs/error.log"
 loglevel = "info"
 timeout = 30
-preload_app = True
-
-
-def post_fork(server, worker):
-    if worker.worker_id == 0:
-        import os
-        if os.getenv("RUN_MAIN", "false") == "true":
-            from app.job import run_job
-            run_job()
--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import logging
+import os
 
 from flask import Flask
 
 from app import create_app
+from app.job import run_job
 
 logging.basicConfig(level=logging.DEBUG)
 
 app: Flask = create_app()
+if os.getenv("RUN_MAIN", "false") == "true":
+    run_job()
 
 if __name__ == "__main__":
     app.run()
