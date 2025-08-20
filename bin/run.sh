01a1677974042a7d64e7e8c669fea533662c9d69
dr-x <dr-x@mail.com>
2025-08-20 14:30:02 +0800

    feat: db creat script init

--- bin/run.sh
+++ bin/run.sh
@@ -4,11 +4,14 @@ set -e
 if [ ! -d "migrations" ]; then
     echo "初始化数据库迁移目录"
     flask db init
-    echo "对比差异"
-    flask db migrate -m "init"
-    echo "执行数据库迁移"
-    flask db upgrade
 fi
+
+
+echo "对比差异"
+flask db migrate -m "init"
+echo "执行数据库迁移"
+flask db upgrade
+
 
 echo "启动 Gunicorn"
 gunicorn -c gunicorn.py run:app
