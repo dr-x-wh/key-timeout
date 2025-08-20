f9af61e6cb20958bb6fa954171316c7bd3b2f104
dr-x <dr-x@mail.com>
2025-08-20 11:39:10 +0800

    fix: import

--- app/job/__init__.py
+++ app/job/__init__.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 
 scheduler: Optional[BackgroundScheduler] = None
 
@@ -13,11 +14,11 @@ def run_job():
         scheduler = BackgroundScheduler(daemon=True)
 
         from app.job.key_timeout import run as run_key_timeout
-        scheduler.add_job(func=run_key_timeout, trigger=CronTrigger(minute=0), id='run_key_timeout',
+        scheduler.add_job(func=run_key_timeout, trigger=IntervalTrigger(minutes=1), id='run_key_timeout',
                           replace_existing=True)
 
         from app.job.power_outage import run as run_power_outage
-        scheduler.add_job(func=run_power_outage, trigger=CronTrigger(minute=0), id='run_power_outage',
+        scheduler.add_job(func=run_power_outage, trigger=IntervalTrigger(minutes=1), id='run_power_outage',
                           replace_existing=True)
 
         scheduler.start()
