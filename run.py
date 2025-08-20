72b2804f2f83f20f7dda43fad84cdf819514be8a
dr-x <dr-x@mail.com>
2025-08-20 10:52:19 +0800

    fix: import

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
+import logging
+
 from flask import Flask
 
 from app import create_app
 from app.job import run_job
 
+logging.basicConfig(level=logging.DEBUG)
+
 app: Flask = create_app()
-app.logger.warning(app.config.get('RUN_MODE'))
+logging.warning(app.config.get('RUN_MODE'))
 if app.config.get('RUN_MODE') != 'migration':
     run_job()
 
