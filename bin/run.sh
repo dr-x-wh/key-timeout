7418ae9997d926cc8643d2543ad56d9214f623c5
dr-x <dr-x@mail.com>
2025-08-20 11:52:29 +0800

    fix: import

--- bin/run.sh
+++ bin/run.sh
@@ -4,10 +4,9 @@ set -e
 if [ ! -d "migrations" ]; then
     echo "初始化数据库迁移目录"
     flask db init
+    echo "执行数据库升级"
+    flask db upgrade
 fi
 
-echo "执行数据库升级"
-flask db upgrade
-
 echo "启动 Gunicorn"
-exec gunicorn -c gunicorn.py run:app
+gunicorn -c gunicorn.py run:app
