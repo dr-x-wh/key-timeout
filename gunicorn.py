02be2855c13706eb5d3778ce60ccb5e67102657c
dr-x <dr-x@mail.com>
2025-08-20 11:29:21 +0800

    fix: import

--- gunicorn.py
+++ gunicorn.py
@@ -1,3 +1,5 @@
+import os
+
 bind = "0.0.0.0:9009"
 workers = 9
 accesslog = "logs/access.log"
@@ -9,7 +11,6 @@ preload_app = True
 
 def post_fork(server, worker):
     if worker.worker_id == 0:
-        import os
-        from app.job import run_job
         if os.getenv("RUN_MAIN", "false") == "true":
+            from app.job import run_job
             run_job()
