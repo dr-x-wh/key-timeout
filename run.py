e1f8e362119207723e8c2100b05d1f1c572deac7
dr-x <dr-x@mail.com>
2025-08-20 13:57:05 +0800

    fix: import

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 from flask import Flask
@@ -6,10 +5,9 @@ from flask import Flask
 from app import create_app
 from app.job import run_job
 
-logging.basicConfig(level=logging.DEBUG)
-
 app: Flask = create_app()
 if os.getenv("RUN_MAIN", "false") == "true":
+    app.logger.info("job is running")
     run_job()
 
 if __name__ == "__main__":
