9d0a7130d430a70e1e04feddf86c57030f030acf
dr-x <dr-x@mail.com>
2025-08-20 14:56:04 +0800

    feat: db creat script init

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export RUN_MAIN=false
 echo "对比差异"
 flask db migrate -m "init"
 echo "执行数据库迁移"
 flask db upgrade
 
 echo "启动 Gunicorn"
+export RUN_MAIN=true
 gunicorn -c gunicorn.py run:app
