61c005b2d948dc726b7fda382da7acc7bdf80a00
dr-x <dr-x@mail.com>
2025-08-20 12:05:14 +0800

    fix: import

--- bin/run.sh
+++ bin/run.sh
@@ -4,8 +4,9 @@ set -e
 if [ ! -d "migrations" ]; then
     echo "初始化数据库迁移目录"
     flask db init
-    echo "执行数据库升级"
+    echo "对比差异"
     flask db migrate -m "init"
+    echo "执行数据库迁移"
     flask db upgrade
 fi
 
